bafe3d10087160e157aebf678b8eba9c60fa8889
novrain <novrainn@gmail.com>
2019-12-29 11:46:24 +0800

    :hammer: add prepublish, change build to lib

--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,6 @@ const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPl
 
 module.exports = {
     configureWebpack: {
-        plugins: [new BundleAnalyzerPlugin()]
+        plugins: [new BundleAnalyzerPlugin({ analyzerMode: 'static' })]
     }
 }
